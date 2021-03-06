2aa1e1e7836becf9b6c0b451b439916d783975d5
kalyani-Bhimavarapu <kalyanibhimavarapu5@gmail.com>
2021-03-06 14:33:02 +0530

    captured the network responce of Enam market data using Puppeteer

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+const results = [];
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    const page = await browser.newPage()
+    await page.goto("https://enam.gov.in/web/dashboard/trade-data", {
+        waitUntil: 'networkidle2'
+    });
+    await page.select('select[id="min_max_state"]', '276')
+    await page.$eval('input#min_max_apmc_from_date', el => el.value = '2021-03-02');
+    await page.$eval('input#min_max_apmc_to_date', el => el.value = '2021-03-02');
+    await page.click('#refresh')
+
+    page.on('response', async (response) => {
+        if (response.url() == "https://enam.gov.in/web/Ajax_ctrl/trade_data_list") {
+            console.log('XHR response received');
+           console.log(await response.json());
+
+        }
+    });
+})()
